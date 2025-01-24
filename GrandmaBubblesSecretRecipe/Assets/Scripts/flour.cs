2d6de9eb5bb0290f96d11e4709bc18a965f6e325
idan8426 <idan8426@gmail.com>
2025-01-24 17:56:09 +0200

    Implemented flour.

---
+++ GrandmaBubblesSecretRecipe/Assets/Scripts/flour.cs
@@ -0,0 +1,134 @@
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class flour : MonoBehaviour, IPossesable
+{
+    [SerializeField]
+    private float movementSpeed;
+
+    [SerializeField]
+    private float maxMovementSpeed;
+
+    [SerializeField]
+    private float ariseForce;
+
+    [SerializeField]
+    private float jumpForce;
+
+    [SerializeField]
+    private GameObject chickenView;
+
+    [SerializeField]
+    private Rigidbody2D flourRigidbody;
+
+    [SerializeField]
+    private float flourSpreadDistance;
+
+    private GameObject mainView;
+    private bool grounded;
+
+    public IngredientType IngerdientType => IngredientType.Chicken;
+    public void OnAction()
+    {
+        if (!grounded)
+        {
+            return;
+        }
+
+        grounded = false;
+        flourRigidbody.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+    }
+
+    public void OnMove(Vector2 moveDirection)
+    {
+        var currentSpeed = flourRigidbody.linearVelocity.magnitude;
+        var moveStep = moveDirection * movementSpeed * Time.deltaTime;
+        moveStep.y = 0.0f;
+        var actualForce = moveStep * (1 - currentSpeed / maxMovementSpeed);
+        flourRigidbody.AddForce(actualForce);
+    }
+
+    public void OnPossessed(PlayerController playerController)
+    {
+        mainView = Instantiate(chickenView, transform);
+    }
+
+    public void OnDeath()
+    {
+        Destroy(mainView);
+        Destroy(gameObject);
+    }
+
+    public Vector3 GetCenterPosition()
+    {
+        return mainView.transform.position;
+    }
+
+    public Vector3 GetPredictedPosition(Vector2 moveDirection)
+    {
+        var velocity = moveDirection * movementSpeed * Time.deltaTime;
+        return transform.position + new Vector3(velocity.x, velocity.y, 0) * Time.deltaTime * 5;
+    }
+
+    private void Update()
+    {
+        if (grounded)
+        {
+            return;
+        }
+
+        var hits = Physics2D.RaycastAll(flourRigidbody.position, Vector2.down, flourSpreadDistance);
+        if(hits.Any(h => h.transform.CompareTag("Ingredient")))
+        {
+            var ingredients = hits.Where(h => h.transform.CompareTag("Ingredient"));
+            foreach (var ingredient in ingredients)
+            {
+                if(!ingredient.rigidbody.TryGetComponent<FlourPowder>(out _)){
+                    ingredient.rigidbody.AddComponent<FlourPowder>();
+                }
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        Arise();
+    }
+
+    public void Arise()
+    {
+        var maxAngle = 1f;
+        var lookDirection = flourRigidbody.transform.up;
+        var cross = Vector3.Cross(Vector2.up, lookDirection);
+        var sign = Mathf.Sign(cross.z);
+
+        var angle = Vector2.Angle(Vector2.up, lookDirection);
+
+        angle *= sign;
+
+        var absAngle = Mathf.Abs(angle);
+
+        if (absAngle > maxAngle)
+        {
+            flourRigidbody.AddTorque(-sign * Time.deltaTime * ariseForce);
+        }
+    }
+    public void OnActionDown()
+    {
+
+    }
+
+    public void OnActionUp()
+    {
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Platform"))
+        {
+            grounded = true;
+        }
+    }
+}
