55d8573ce7ac46a56fb880a5aca3329c8d3eb829
idan8426 <idan8426@gmail.com>
2025-01-25 04:13:31 +0200

    Implemented pot and fixed collision issues.

--- GrandmaBubblesSecretRecipe/Assets/Scripts/DeathFloor.cs
+++ GrandmaBubblesSecretRecipe/Assets/Scripts/DeathFloor.cs
@@ -1,15 +1,13 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class Slicer : MonoBehaviour
+public class DeathFloor : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.attachedRigidbody.CompareTag("Ingredient"))
         {
             var playerController = collision.GetComponentInParent<PlayerController>();
-            collision.attachedRigidbody.AddComponent<Sliced>();
-            playerController.Die(true);
+            playerController.Die();
         }
     }
 }
--- GrandmaBubblesSecretRecipe/Assets/Scripts/Pot.cs
+++ GrandmaBubblesSecretRecipe/Assets/Scripts/Pot.cs
@@ -1,14 +1,12 @@
-using Unity.VisualScripting;
 using UnityEngine;
 
-public class Slicer : MonoBehaviour
+public class Pot : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.attachedRigidbody.CompareTag("Ingredient"))
         {
             var playerController = collision.GetComponentInParent<PlayerController>();
-            collision.attachedRigidbody.AddComponent<Sliced>();
             playerController.Die(true);
         }
     }
